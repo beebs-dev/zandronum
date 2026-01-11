6907717c17caccdb733653a115a1ee45d2940f72
Thomas Havlik <thavlik@protonmail.com>
2026-01-10 21:51:10 -0500

    ...

--- server.sh
+++ server.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -euo pipefail
+
 echo "Game ID: ${GAME_ID:-unset}"
 echo "Using IWAD: $IWAD"
 echo "Warp Level: ${WARP:-unset}"
 echo "Using Game Skill: ${SKILL:-unset}"
 echo "Using Data Root: ${DATA_ROOT:-unset}"
-PORT="${GAME_PORT:-10666}"
-CMD=(
-    /opt/zandronum/zandronum-server
-)
-# Zandronum will look for wads in the cwd
+
 cd "$DATA_ROOT"
-echo ">>> ${CMD[*]}"
-exec "${CMD[@]}"
+
+SERVER=(/opt/zandronum/zandronum-server)
+
+echo ">>> ${SERVER[*]}"
+"${SERVER[@]}" &
+pid=$!
+
+term() {
+  echo "Got SIGTERM, forwarding to server pid=$pid"
+  kill -TERM "$pid" 2>/dev/null || true
+
+  # If TERM doesn’t work for zandronum, try INT instead:
+  # kill -INT "$pid" 2>/dev/null || true
+}
+
+trap term TERM INT
+wait "$pid"
